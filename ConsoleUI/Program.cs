f106b39b4624605649a2bc92770f41b5fabf62a8
Sanchklyc <halitenes.kalayci@gmail.com>
2021-03-28 02:52:41 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICarService carService = new CarManager(new InMemoryCarDal());
+
+            carService.Add(new Car { Id = 1001, BrandId = 1, ColorId = 2, DailyPrice = 500, Description = "Peugeout Partner", ModelYear = 2016 });
+            carService.Add(new Car { Id = 1002, BrandId = 4, ColorId = 2, DailyPrice = 120, Description = "Opel Corsa", ModelYear = 2020 });
+            carService.Delete(new Car { Id = 1001 });
+            carService.Update(new Car { Id = 1002, BrandId = 4, ColorId = 2, DailyPrice = 140, Description = "Opel Corsa", ModelYear = 2020 });
+
+            foreach (Car car in carService.GetAll())
+            {
+                Console.WriteLine($"\n {car.Id} kodlu araba bilgileri : {car.Description} , Günlük ücret {car.DailyPrice} , Model Yılı {car.ModelYear} \n");
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,50 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> { 
+            new Car{Id=1,ModelYear=2012,Description="BMW M5",DailyPrice=200,ColorId=1,BrandId=1},
+            new Car{Id=2,ModelYear=2019,Description="Mercedes Vito",DailyPrice=300,ColorId=1,BrandId=2},
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            _cars.Remove(_cars.SingleOrDefault(c=>c.Id == car.Id));
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public Car GetById(int carId)
+        {
+            return _cars.SingleOrDefault(c => c.Id == carId);
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
